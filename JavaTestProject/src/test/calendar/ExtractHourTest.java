7e30e40d1e3193384a2ed3fb949420c717da5187
begemot57 <begemot57@hotmail.com>
2014-08-10 15:43:16 +0100

    add new zip util class

--- JavaTestProject/src/test/calendar/ExtractHourTest.java
+++ JavaTestProject/src/test/calendar/ExtractHourTest.java
@@ -2,15 +2,19 @@ package test.calendar;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 public class ExtractHourTest {
 
 	public static void main(String[] args) {
 		Calendar cal;
     	SimpleDateFormat sdf1 = new SimpleDateFormat("E yyyy.MM.dd HH:mm:ss");
+//    	cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
     	cal = Calendar.getInstance();
 		System.out.println("current time: "+sdf1.format(cal.getTime()));
 		System.out.println("hours: "+cal.get(Calendar.HOUR_OF_DAY));
+		System.out.println("time zone: "+cal.getTimeZone().getDisplayName());
+		
 		
 		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy.MM.dd");
 		System.out.println("date: "+sdf2.format(cal.getTime()));
