0d7b6858fa0621ea2133a524e912a07e34949c4d
begemot57 <begemot57@hotmail.com>
2014-08-13 09:28:09 +0100

    tz test

--- JavaTestProject/src/test/calendar/ExtractHourTest.java
+++ JavaTestProject/src/test/calendar/ExtractHourTest.java
@@ -9,8 +9,8 @@ public class ExtractHourTest {
 	public static void main(String[] args) {
 		Calendar cal;
     	SimpleDateFormat sdf1 = new SimpleDateFormat("E yyyy.MM.dd HH:mm:ss");
-//    	cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-    	cal = Calendar.getInstance();
+    	cal = Calendar.getInstance(TimeZone.getTimeZone("Greenwich Mean Time"));
+//    	cal = Calendar.getInstance();
 		System.out.println("current time: "+sdf1.format(cal.getTime()));
 		System.out.println("hours: "+cal.get(Calendar.HOUR_OF_DAY));
 		System.out.println("time zone: "+cal.getTimeZone().getDisplayName());
