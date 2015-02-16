3c0f36b11b8c58efb76c8a3f55684c8d785f5152
begemot57 <begemot57@hotmail.com>
2015-02-16 16:49:36 +0000

    change replace string

--- JavaTestProject/src/test/misc/StringTest.java
+++ JavaTestProject/src/test/misc/StringTest.java
@@ -7,12 +7,19 @@ public class StringTest {
 	 */
 	public static void main(String[] args) {
 		StringTest test = new StringTest();
-		test.replaceTest();
+		String str = "cmd : /opt/IBM/JazzSM/profile//bin/wsadmin.sh -lang jython -user smadmin -password smadmin -f /tmp//libertyinstall_root/export_key.jy /tmp//libertyinstall_root/pi_dash_ltpa.keys passw0rd";
+		System.out.println(test.replaceTest(str));
+		str = "root@nc9042036111.tivlab.raleigh.ibm.com  /opt/IBM/JazzSM/profile//bin/wsadmin.sh -user smadmin -password smadmin -lang jython -f /tmp//libertyinstall_root/export_keys.jy \"passw0rd\"";
+		System.out.println(test.replaceTest(str));
 	}
 	
-	void replaceTest(){
-		String str = "/opt/IBM/JazzSM/profile//bin/wsadmin.sh -lang jython -user smadmin -password smadmin -f /tmp//libertyinstall_root/delete_signer_cert.jy";
-		System.out.println(str.replaceAll("pass[^\\s]+[\\s]+[^\\s]+", "password XXXXXX"));
+	String replaceTest(String str){
+		str = str.replaceAll("pass[^\\s]+[\\s]+[^\\s]+", "password XXXXXX");
+		
+		str = str.replaceAll("export_keys.jy[\\s]+[^\\s]+", "export_keys.jy XXXXXX");
+		if(str.contains("jython"))
+			str = str.replaceAll("pi_dash_ltpa.keys[\\s]+[^\\s]+", "pi_dash_ltpa.keys XXXXXX");
+		return str;
 	}
 
 }
