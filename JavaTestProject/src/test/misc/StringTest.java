5bff9e2d214311741ac59b136e61a7bcac5c4c81
begemot57 <begemot57@hotmail.com>
2015-04-21 12:08:32 +0100

    add print string test

--- JavaTestProject/src/test/misc/StringTest.java
+++ JavaTestProject/src/test/misc/StringTest.java
@@ -7,10 +7,12 @@ public class StringTest {
 	 */
 	public static void main(String[] args) {
 		StringTest test = new StringTest();
-		String str = "cmd : /opt/IBM/JazzSM/profile//bin/wsadmin.sh -lang jython -user smadmin -password smadmin -f /tmp//libertyinstall_root/export_key.jy /tmp//libertyinstall_root/pi_dash_ltpa.keys passw0rd";
-		System.out.println(test.replaceTest(str));
-		str = "root@nc9042036111.tivlab.raleigh.ibm.com  /opt/IBM/JazzSM/profile//bin/wsadmin.sh -user smadmin -password smadmin -lang jython -f /tmp//libertyinstall_root/export_keys.jy \"passw0rd\"";
-		System.out.println(test.replaceTest(str));
+//		String str = "cmd : /opt/IBM/JazzSM/profile//bin/wsadmin.sh -lang jython -user smadmin -password smadmin -f /tmp//libertyinstall_root/export_key.jy /tmp//libertyinstall_root/pi_dash_ltpa.keys passw0rd";
+//		System.out.println(test.replaceTest(str));
+//		str = "root@nc9042036111.tivlab.raleigh.ibm.com  /opt/IBM/JazzSM/profile//bin/wsadmin.sh -user smadmin -password smadmin -lang jython -f /tmp//libertyinstall_root/export_keys.jy \"passw0rd\"";
+//		System.out.println(test.replaceTest(str));
+		
+		test.printString(null);
 	}
 	
 	String replaceTest(String str){
@@ -20,6 +22,10 @@ public class StringTest {
 		if(str.contains("jython"))
 			str = str.replaceAll("pi_dash_ltpa.keys[\\s]+[^\\s]+", "pi_dash_ltpa.keys XXXXXX");
 		return str;
+	}
+	
+	void printString(String str){
+		System.out.println("str: "+str);
 	}
 
 }
