02e35ed1467d53400ec875020eda8a393480d09c
begemot57 <begemot57@hotmail.com>
2015-05-22 17:03:43 +0100

    bla

--- JavaTestProject/src/test/files/ReadFile.java
+++ JavaTestProject/src/test/files/ReadFile.java
@@ -3,6 +3,8 @@ package test.files;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
  
 public class ReadFile {
  
@@ -23,5 +25,35 @@ public class ReadFile {
 			e.printStackTrace();
 		} 
  
+	}
+	
+	void changeGranularity(){
+		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Leo\\my\\git\\JavaRepo\\JavaTestProject\\files\\RespTime__1130516000000000__1130601124000000.csv")))
+		{
+ 
+			String sCurrentLine;
+ 
+			int counter = 0;
+			String ts;
+			List<String> tsList = new ArrayList<String>();
+			while ((sCurrentLine = br.readLine()) != null) {
+				counter++;
+				ts = sCurrentLine.split(",")[0];
+				if(!tsList.contains(ts)){
+					if(tsList.size()==6){
+						tsList = new ArrayList<String>();
+						System.out.println(sCurrentLine);
+					}
+					tsList.add(ts);
+				}else{
+					if(tsList.get(0).equals(ts)){
+						System.out.println(sCurrentLine);
+					}
+				}
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
 }
---
+++ JavaTestProject/src/test/misc/DoubleTest.java
@@ -0,0 +1,13 @@
+package test.misc;
+
+public class DoubleTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(Double.parseDouble(null));
+
+	}
+
+}
